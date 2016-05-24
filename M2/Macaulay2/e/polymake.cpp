ad57315c856e8712723e69b62cc1b79347d05d34
Lars Kastner <lars.kastner@yahoo.dk>
2016-05-24 18:02:48 +0200

    Next test

--- M2/Macaulay2/e/polymake.cpp
+++ M2/Macaulay2/e/polymake.cpp
@@ -1,4 +1,24 @@
 #include <polymake/Main.h>
-#include <polymake/Rational.h>
 #include <polymake/Matrix.h>
+#include <polymake/SparseMatrix.h>
+#include <polymake/Rational.h>
+using namespace polymake;
+
+int rawPolymakeConvexHull() {
+  try {
+    const int dim = 4;
+    Main pm;
+    pm.set_application("polytope");
+    perl::Object p("Polytope<Rational>");
+    p.take("VERTICES") << (ones_vector<Rational>() | 
+       3*unit_matrix<Rational>(dim));
+    const Matrix<Rational> f = p.give("FACETS");
+    const Vector<Integer> h = p.give("H_STAR_VECTOR");
+    cout << "facets" << endl << f << endl << "h* " << h << endl;
+  } catch (const std::exception& ex) {
+    std::cerr << "ERROR: " << ex.what() << endl; return 1;
+  }
+  return 0; 
+}
+
 
